4537f7d4babf499b275829b7f2b670940253d963
unknown <tao.hu@cn.corp.seagroup.com>
2020-07-31 18:31:09 +0800

    v2.1.2

---
+++ v2/hello/hello_test.go
@@ -0,0 +1,14 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
+
+func TestRelaxMoment(t *testing.T) {
+	RelaxMoment()
+}
