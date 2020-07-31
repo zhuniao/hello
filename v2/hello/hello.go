0ccbe5b78075be5a8a01dd53a77310eb7f1a6ac7
unknown <tao.hu@cn.corp.seagroup.com>
2020-07-31 19:05:20 +0800

    v2.1.3

--- v2/hello/hello.go
+++ v2/hello/hello.go
@@ -6,8 +6,15 @@ import (
 	"time"
 )
 
-func Hello() string {
-	return "Hello, world."
+func SayHello(language string) {
+	switch language {
+	case "cn":
+		fmt.Println("求知若渴，求贤若愚。")
+	case "en":
+		fmt.Println("Stay hungry,stay foolish.")
+	default:
+		fmt.Println("only support cn and en")
+	}
 }
 
 func randInt64(min, max int64) int64 {
